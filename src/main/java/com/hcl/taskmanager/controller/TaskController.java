2e3c6650f17f2e78fd34cc65c7e9e32ac016c22e
Phong Van Nguyen <phong_nguyen@hcl.com>
2021-02-19 10:37:44 -0500

    add exception folder

--- src/main/java/com/hcl/taskmanager/controller/TaskController.java
+++ src/main/java/com/hcl/taskmanager/controller/TaskController.java
@@ -5,12 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.hcl.taskmanager.entity.Task;
@@ -61,7 +59,7 @@ public class TaskController {
 		return "redirect:/";
 	}
 	
-	@RequestMapping(value = "/delete-task/{id}", method = RequestMethod.DELETE)
+	@RequestMapping("/delete-task/{id}")
 	public String deleteTask(@PathVariable(name = "id") long id) {
 		taskService.deleteTask(id);
 		
